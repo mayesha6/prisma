1adc3fba3ed50f69ffc3dafb0c833ef2f1c055fc
mayesha <soumyrhmn234@gmail.com>
2025-09-15 09:41:21 +0600

    insert data in table

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from "@prisma/client"
+
+
+
+const prisma = new PrismaClient()
+
+async function main() {
+    const result = await prisma.users.create({
+        data: {
+            name: "Tania Tabassum",
+            email:"tania@gmail.com"
+        }
+    })    
+    console.log(result)
+
+   
+}
+
+main()
+//   .then(async () => {
+//     await prisma.$disconnect()
+//   })
+//   .catch(async (e) => {
+//     console.error(e)
+//     await prisma.$disconnect()
+//     process.exit(1)
+//   })
