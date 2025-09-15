6a987aa6ff50e36d473ced824a1dbd3ff27ff0c0
mayesha <soumyrhmn234@gmail.com>
2025-09-15 09:43:21 +0600

    get all data from table

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,16 @@ import { PrismaClient } from "@prisma/client"
 const prisma = new PrismaClient()
 
 async function main() {
-    const result = await prisma.users.create({
-        data: {
-            name: "Tania Tabassum",
-            email:"tania@gmail.com"
-        }
-    })    
-    console.log(result)
+    // const result = await prisma.users.create({
+    //     data: {
+    //         name: "Tania Tabassum",
+    //         email:"tania@gmail.com"
+    //     }
+    // })    
+    // console.log(result)
 
+    const userData = await prisma.users.findMany()
+    console.log(userData)
    
 }
 
